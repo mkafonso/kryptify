571499594d5add323e14cc1452146833187e5787
makaya2afonso <makaya2afonso@gmail.com>
2024-01-25 21:37:38 -0300

    test account entity

---
+++ __tests__/entities/account_test.go
@@ -0,0 +1,38 @@
+package entities_test
+
+import (
+	"kryptify/entities"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAccountEntity_TestCreateNewAccount(t *testing.T) {
+	account, err := entities.NewAccount("John Doe", "john@email.com", "test1234")
+	assert.NoError(t, err)
+	assert.NotNil(t, account)
+}
+
+func TestAccountEntity_TestEmptyName(t *testing.T) {
+	_, err := entities.NewAccount("", "john@email.com", "test1234")
+	assert.Error(t, err)
+	assert.Equal(t, err.Error(), "missing required fields in the JSON object")
+}
+
+func TestAccountEntity_TestEmptyEmail(t *testing.T) {
+	_, err := entities.NewAccount("John Doe", "", "test1234")
+	assert.Error(t, err)
+	assert.Equal(t, err.Error(), "missing required fields in the JSON object")
+}
+
+func TestAccountEntity_TestEmptyPassword(t *testing.T) {
+	_, err := entities.NewAccount("John Doe", "john@email.com", "")
+	assert.Error(t, err)
+	assert.Equal(t, err.Error(), "missing required fields in the JSON object")
+}
+
+func TestAccountEntity_TestShortPassword(t *testing.T) {
+	_, err := entities.NewAccount("John Doe", "john@email.com", "test")
+	assert.Error(t, err)
+	assert.Equal(t, err.Error(), "password length must be at least 8 characters")
+}
